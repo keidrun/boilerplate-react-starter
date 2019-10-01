f6a494552fd419e1839f58f59ba98876c6d5fe31
keidrun <kssk96@gmail.com>
2019-10-01 23:45:46 +0900

    Add base react settings

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { hot } from 'react-hot-loader/root';
+import './App.css';
+
+type Props = {
+  initialCount: number,
+};
+
+function App({ initialCount }: Props) {
+  const [count, setCount] = useState(initialCount);
+
+  return (
+    <div className="App" data-test="component-app">
+      <header className="App-header">
+        <p>My React App without Create React App!</p>
+        <p>Count: {count}</p>
+        <div>
+          <button onClick={() => setCount(count + 1)}>+1</button>
+          <button onClick={() => setCount(count - 1)}>-1</button>
+          <button onClick={() => setCount(initialCount)}>clear</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+App.defaultProps = {
+  initialCount: 0,
+};
+
+export default hot(App);
---
+++ webpack.config.babel.js
@@ -0,0 +1,68 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const outputPath = path.resolve(__dirname, 'build');
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash].js',
+    path: outputPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: outputPath,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+};
